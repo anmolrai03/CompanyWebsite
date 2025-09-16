b43f03f2c1ab669f476fc8738aece996a9bd3839
Anmol Rai <anmolrai0307@gmail.com>
2025-09-16 12:12:38 +0530

    code base restrutrue for buttons in utils

---
+++ client/src/assets/buttons/buttons.js
@@ -0,0 +1,7 @@
+import PlayIcon from "./PlayIcon";
+import CloseIcon from "./CloseIcon";
+import MuteIcon from "./MuteIcon";
+import PauseIcon from "./PauseIcon";
+import VolumeIcon from "./VolumeIcon";
+
+export {PlayIcon, CloseIcon, MuteIcon, PauseIcon, VolumeIcon};
