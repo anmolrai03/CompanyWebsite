8359163bf2a28a9fb69866cd62f4a1b770a88d7a
AnmolChauhan <acanmolchauhan1234@gmail.com>
2025-09-19 00:30:31 +0530

    update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 import mongoose from "mongoose";
 import express from "express";
-import connectdb from "./db/index.js";
+import connectdb from "./db/db.js";
 
 dotenv.config({
     path: './env'
